d93c5017697060def0846355440ae3c409796871
jaskirat1208 <jaskiratsingh1208@gmail.com>
2018-02-10 15:42:58 +0530

    My commit

--- asgn2/test_allocate_reg.cpp
+++ asgn2/test_allocate_reg.cpp
@@ -4,12 +4,25 @@
 using namespace std;
 
 int main(){
-	RegisterDescriptor registerDescriptor1 = RegisterDescriptor();
-	Register r = registerDescriptor1.findEmptyRegister(); 		//finds an empty register
-	SymbolTableEntry s1;
-	registerDescriptor1.modify(r,&s1);
-	cout<<registerDescriptor1.findEmptyRegister();
+	registerDescriptor = RegisterDescriptor();
+	SymbolTableEntry s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16;
+	registerDescriptor.modify(RAX, &s1);
+	registerDescriptor.modify(RBX, &s2);
+	registerDescriptor.modify(RCX, &s3);
+	registerDescriptor.modify(RDX, &s4);
+	registerDescriptor.modify(RSI, &s5);
+	registerDescriptor.modify(RDI, &s6);
+	registerDescriptor.modify(RBP, &s7);
+	registerDescriptor.modify(RSP, &s8);
+	registerDescriptor.modify(R8, &s9);
+	registerDescriptor.modify(R9, &s10);
+	registerDescriptor.modify(R10,&s11);
+	registerDescriptor.modify(R11,&s12);
+	registerDescriptor.modify(R12,&s13);
+	registerDescriptor.modify(R13,&s14);
+	registerDescriptor.modify(R14,&s15);
+	registerDescriptor.findEmptyRegister();
+	//modify(R15,&s16);
 	return 0;
 
 }
-
