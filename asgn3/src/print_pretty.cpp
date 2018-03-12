288cd07cdb6c954218734ef7c6003303d096d47b
jaskirat1208 <jaskiratsingh1208@gmail.com>
2018-03-12 20:38:02 +0530

    Hi budddy

--- asgn3/src/print_pretty.cpp
+++ asgn3/src/print_pretty.cpp
@@ -1,4 +1,10 @@
 #include <bits/stdc++.h>
+
+#define PATH_TO_FILE "parser.html"
+#define COLOR_1 "red"
+#define COLOR_2 "green"
+#define COLOR_3 "yellow"
+
 using namespace std;
 
 vector<string> split(const string& s, char delimiter) {
@@ -29,13 +35,59 @@ int getRightMostNonTerminalIndex(vector<string> v) {
 	return -1;
 }
 
-void print_pretty(vector<string> v) {
+void print_code(std::vector<string> v){
 
-    cout<<"<!DOCTYPE html>\n<html>\n<head>\n\t<title>CODE AND GRAMMAR</title>\n</head>\n<body><ol>";
-	string currString = v[0];
-	cout <<"<li><b>"<< currString<<"</b>" << endl;
+}
+void print_pretty(vector<string> v) {
+	ofstream html_file;
+    string tmp = R"(<div class="box">
+	<h2 align="center"> Printing the parse tree </h2>
+	<p>At vero eos et accusamus et iusto odio dignissimos ducimus:</p>
+	<ol>)";
+	
+	html_file.open(PATH_TO_FILE);
+    string currString = v[0];
+    html_file<<R"(<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		div.box {
+				margin: 1.6em 0 1em 0;
+		    padding: 0 0 0 1em;
+				border: 2px solid #7db447;
+			}
+			ol {
+				margin: 1em;
+				padding: 0.1em 0 0.8em 1.2em;
+			}
+			.no-bg, .bg {
+				font-family: "DejaVu Sans", sans-serif;
+				font-weight: bold;z
+				padding: 0.1em 0 0.8em 1em;
+			}
+			.bg {
+				background-color: #e3f3d4;
+			}
+			.no-bg p, .bg p {
+				font-family: "DejaVu Serif", serif;
+			}
+
+	</style>
+</head>
+<body>
+)";
+    html_file<<tmp;
+	// html_file <<"<li class = \"bg\"><b>"<< currString<<"</b></li>" << endl;
 	vector<string> vecString = split(currString, ' ');
+	string string_before_last_non_terminal="",last_non_terminal="",residual="";
+	for (int i = 0; i < vecString.size()-1; ++i)
+	{
+		string_before_last_non_terminal+=vecString[i] + " ";
+	}
+	html_file<<"<li class = \"bg\"><font size=\"3\" color=\"red\"> "<<string_before_last_non_terminal<<"  </font>  <font size=\"3\" color=\"green\"> "<<vecString[vecString.size()-1]<<"</font></li>\n";
 	int index, currIndex = 1;
+	int id=0;								//only for formatting sake
 	while ((index = getRightMostNonTerminalIndex(vecString)) != -1) {
 		currString = "";
 		for (int i = 0; i < vecString.size(); i++){
@@ -55,26 +107,33 @@ void print_pretty(vector<string> v) {
 			}
 		}
 		vecString = split(currString, ' ');
-        string to_print="";
-        string string_before_last_non_terminal="",last_non_terminal="",residual="";
+        string_before_last_non_terminal="",last_non_terminal="",residual="";
         int tmp_index = getRightMostNonTerminalIndex(vecString);
-        for(int i=0;i<vecString.size();i++){
+        for(int i=2;i<vecString.size();i++){
             if(i<tmp_index){
                 string_before_last_non_terminal+=vecString[i]+" ";
             }
             else if(i>tmp_index){
                 residual+=vecString[i]+" ";
             }
-            if(tmp_index!=-1)                   //case when all are terminals :)
+            if(tmp_index!=-1)
                 last_non_terminal=vecString[tmp_index]+" ";
         }
-        string_before_last_non_terminal = "<font size=\"3\" color=\"red\">"+string_before_last_non_terminal+"</font>";
+        string_before_last_non_terminal = "<font size=\"3\" color=\"red\"> "+vecString[0]+" "+vecString[1]+" "+string_before_last_non_terminal+"</font>";
         residual = "<font size=\"3\" color=\"yellow\">"+residual+"</font>";
         last_non_terminal = "<font size=\"3\" color=\"green\">"+ last_non_terminal +"</font>";
-        cout<<"<li>"<<string_before_last_non_terminal<<" "<<last_non_terminal<<" "<<residual<<endl;
-		// cout<<"RULE USED: "<<v[currIndex]<<"-------------------------------------------------------------"<<endl;
-
+        if (id%2==1)
+        {
+       		html_file<<"<li class = \"bg\">"<<string_before_last_non_terminal<<" "<<last_non_terminal<<" "<<residual<<"</li>\n";
+        }
+        else{
+        	html_file<<"<li class = \"no-bg\">"<<string_before_last_non_terminal<<" "<<last_non_terminal<<" "<<residual<<"</li>\n";
+        }
+        // cout<<"RULE USED: "<<v[currIndex]<<"-------------------------------------------------------------"<<endl;
+        id++;
 		currIndex++;
 	}
-    cout<<"</ol></body>\n</html>";
+    html_file<<"</ol></div>";
+   
+
 }
