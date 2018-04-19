b5fd32c3f48c603c1f9d797820157c6262117659
jaskirat1208 <jaskiratsingh1208@gmail.com>
2018-04-19 13:49:17 +0530

    arrwrite complete

--- asgn2/asmOut/result.s
+++ asgn2/asmOut/result.s
@@ -1,12 +1,14 @@
 .data
 	t3:	.quad 0
+	t2:	.quad 0
 	arr: 
 		.quad	7
 		.quad	2
 		.quad	4
 		.quad	5
 		.quad	8
-	t2:	.quad 0
+	b:	.quad 0
+	t4:	.quad 0
 	str:	.string "%d\n"
 	scan_str:	.string "%d"
 .text
@@ -17,8 +19,20 @@ label0:
 	add $16, %RBX
 	push (%RBX)
 	pop t2
-	movq $5, %RCX
-	movq %RCX, t3
+	movq $1, %RCX
+	movq %RCX, b
+
+	movq %RCX, b
+	movq $arr, %RCX
+	add $8, %RCX
+	push b
+	pop (%RCX)
+	movq $arr, %RSI
+	add $8, %RSI
+	push (%RSI)
+	pop t4
+	movq $5, %RDI
+	movq %RDI, t3
 
 	pushq %RDI
 	pushq %RSI
@@ -40,6 +54,21 @@ label0:
 	pushq %RAX
 	movq $str, %RDI
 	movq t2, %RSI
+	movq $0, %RAX
+	pushq %R10
+	pushq %R11
+	call printf
+	popq %R11
+	popq %R10
+	popq %RAX
+	popq %RSI
+	popq %RDI
+
+	pushq %RDI
+	pushq %RSI
+	pushq %RAX
+	movq $str, %RDI
+	movq t4, %RSI
 	movq $0, %RAX
 	pushq %R10
 	pushq %R11
